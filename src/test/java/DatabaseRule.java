2de1c6406d008c5db925825066660ec56b3eaa13
chitra <chitra.atmakuru@gmail.com>
2015-08-29 15:02:19 -0700

    Add finished integration tests and README.txt

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -4,8 +4,8 @@ import org.sql2o.*;
 public class DatabaseRule extends ExternalResource {
 
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test","chitra","1234");
-    //DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test","null","null");
+    //DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test","chitra","1234");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test","null","null");
    }
 
   protected void after() {
